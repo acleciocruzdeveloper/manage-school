6fa6e9dc31154ac1875429e9d7c59a61c7df924d
sonic-developer <aclecioscruz@gmail.com>
2024-09-08 10:19:36 -0300

    start project menage school

---
+++ src/main/java/br/com/api/manager_school/ManagerSchoolApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.manager_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/manager_school/ManagerSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.manager_school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
